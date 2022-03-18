30ee5ad31c8099a13f0042cc71838b2faa713b07
migueldavid006 <migueldavid006@hotmail.com>
2022-03-18 15:13:18 -0500

    array methods async

---
+++ Dia1/01-let-var.js
@@ -0,0 +1,32 @@
+var nombre = "Osmar";
+let apellido = "Montesinos";
+
+if(true){
+    console.log(nombre);
+    // console.log(apellido);
+    var nombre = "Patrick";
+    let apellido = "Rodriguez";
+    console.log(nombre);
+    console.log(apellido);
+    // ----------------
+    let edad = 20;
+    console.log(edad);
+    for(var i=0;i<6;i++){
+        console.log(i);
+    }
+    console.log(i);
+}
+// console.log(edad);
+console.log(nombre);
+console.log(apellido);
+
+// console.log(edad)
+try{
+    //código que se va a ejecutar y sospecho que me va a generar un error
+    console.log(edad)
+}catch(error){
+    //imprimir el error pero sin detener la ejecución de mi código
+    console.error(error);
+}
+
+console.log(":) :) :)");
